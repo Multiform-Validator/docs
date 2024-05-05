72e2bf50264eb4448a16782d1deb45bc9b475ee4
gabriel-logan <gabrielcoutolima@protonmail.ch>
2024-05-05 06:38:25 -0300

    fix: doc image config

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	images: {
-		unoptimized: false,
+		unoptimized: true,
 		remotePatterns: [
 			{
 				protocol: "https",
--- docs/src/app/(doc-session)/documentation/page.tsx
+++ docs/src/app/(doc-session)/documentation/page.tsx
@@ -14,7 +14,7 @@ export default function Documentation() {
 					<div className="mb-10 flex justify-around">
 						<Link
 							href="/"
-							className="rounded bg-gray-700 px-4 py-2"
+							className="rounded bg-gray-700 px-4 py-2 text-white"
 							id="menu-toggle"
 						>
 							Back to home page
