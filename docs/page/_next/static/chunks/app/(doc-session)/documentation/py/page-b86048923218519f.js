aeb0ba52c640eb3b711bedad25cdac2b88c231b3
gabriel-logan <gabrielcoutolima@protonmail.ch>
2024-05-08 21:33:02 -0300

    chore: create python doc version

---
+++ docs/page/_next/static/chunks/app/(doc-session)/documentation/py/page-b86048923218519f.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[5659],{52:function(n,e,u){Promise.resolve().then(u.t.bind(u,231,23))}},function(n){n.O(0,[231,2971,7023,1744],function(){return n(n.s=52)}),_N_E=n.O()}]);
