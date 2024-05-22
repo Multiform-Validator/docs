2495901e039883b638a924335192a7c0ab40755a
gabriel-logan <gabrielcoutolima@protonmail.ch>
2024-05-22 15:56:48 -0300

    fix: add basePath

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	output: "export",
+	basePath:
+		process.env.NODE_ENV === "development" ? undefined : "/multiform-validator",
 };
 
 export default nextConfig;
