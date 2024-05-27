7827adf1a644c4146406047a0da0f439f87f15a0
gabriel-logan <gabrielcoutolima@protonmail.ch>
2024-05-27 13:40:38 -0300

    chore: add banner test

--- docs/src/app/news/page.tsx
+++ docs/src/app/news/page.tsx
@@ -1,32 +1,59 @@
 import "@/css/news.css";
+import { AddBanner } from "@/components/Adsense";
 import MainBg from "@/components/MainBg";
 
 export default function NewsPage() {
+	const GOOGLE_ADSENSE_CLIENT_ID = process.env.GOOGLE_ADSENSE_CLIENT_ID ?? "";
+	const SlotAd1AndAd2 = "9630566447";
+	const SlotAd3 = "8700628151";
+	const SlotAd4 = "3500719479";
+	const SlotAd5AndAd6 = "7092033017";
+
 	return (
 		<MainBg>
 			<div id="news-page">
 				<div className="div-ad div-ad1">
-					<h1>News1</h1>
+					<AddBanner
+						AdClient={GOOGLE_ADSENSE_CLIENT_ID}
+						AdSlot={SlotAd1AndAd2}
+					/>
 				</div>
 
 				<div className="div-ad div-ad2">
-					<h1>News2</h1>
+					<AddBanner
+						AdClient={GOOGLE_ADSENSE_CLIENT_ID}
+						AdSlot={SlotAd1AndAd2}
+					/>
 				</div>
 
 				<div className="div-ad div-ad3">
-					<h1>News3</h1>
+					<AddBanner
+						AdClient={GOOGLE_ADSENSE_CLIENT_ID}
+						AdSlot={SlotAd3}
+						AdFormat="fluid"
+					/>
 				</div>
 
 				<div className="div-ad div-ad4">
-					<h1>News4</h1>
+					<AddBanner
+						AdClient={GOOGLE_ADSENSE_CLIENT_ID}
+						AdSlot={SlotAd4}
+						AdFormat="autorelaxed"
+					/>
 				</div>
 
 				<div className="div-ad div-ad5">
-					<h1>News5</h1>
+					<AddBanner
+						AdClient={GOOGLE_ADSENSE_CLIENT_ID}
+						AdSlot={SlotAd5AndAd6}
+					/>
 				</div>
 
 				<div className="div-ad div-ad6">
-					<h1>News6</h1>
+					<AddBanner
+						AdClient={GOOGLE_ADSENSE_CLIENT_ID}
+						AdSlot={SlotAd5AndAd6}
+					/>
 				</div>
 			</div>
 		</MainBg>
--- docs/src/components/Adsense/AddBanner.tsx
+++ docs/src/components/Adsense/AddBanner.tsx
@@ -1,19 +1,5 @@
 import Script from "next/script";
 
-interface AdsenseProps {
-	GOOGLE_ADSENSE_CLIENT_ID: string | undefined;
-}
-
-export default function Adsense({ GOOGLE_ADSENSE_CLIENT_ID }: AdsenseProps) {
-	return (
-		<script
-			async
-			src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${GOOGLE_ADSENSE_CLIENT_ID}`}
-			crossOrigin="anonymous"
-		/>
-	);
-}
-
 interface AddBannerProps {
 	AdClient: string;
 	AdSlot: string;
@@ -21,8 +7,7 @@ interface AddBannerProps {
 	FullWidthResponsive?: boolean;
 }
 
-// TEST
-export function AddBanner({
+export default function AddBanner({
 	AdClient,
 	AdSlot,
 	AdFormat = "auto",
