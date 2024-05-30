9c9ae7b74caa069b956efc7dfa341c43fff03d9c
gabriel-logan <gabrielcoutolima@protonmail.ch>
2024-05-29 23:05:58 -0300

    chore: adjusting interceptor

--- docs/src/components/Intercepter/index.tsx
+++ docs/src/components/Intercepter/index.tsx
@@ -1,13 +1,12 @@
 "use client";
 
 import { redirect } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 
 import { getBrowserLang } from "../Internationalization";
 
 export default function Intercepter() {
 	const browserLang = getBrowserLang();
-	const [lang, setLang] = useState("en");
 
 	useEffect(() => {
 		const verify =
@@ -15,22 +14,11 @@ export default function Intercepter() {
 				(browserLang[0] === "pt" || browserLang[0] === "pt-BR")) ||
 			(browserLang && (browserLang[1] === "pt" || browserLang[1] === "pt-BR"));
 		if (verify) {
-			setLang("pt");
 			document.documentElement.lang = "pt";
 
 			redirect("/pt");
 		}
 	}, []);
 
-	return (
-		<>
-			{lang === "pt" && (
-				<div className="min-h-screen">
-					<p className="text-center text-white">
-						Retornando para a página em português...
-					</p>
-				</div>
-			)}
-		</>
-	);
+	return null;
 }
