2e0fffd8e39e63e1fff820bb1b491053eb63e858
gabriel-logan <gabrielcoutolima@protonmail.ch>
2024-05-31 23:27:02 -0300

    refactor: analytics and speed

--- docs/src/app/[locale]/layout.tsx
+++ docs/src/app/[locale]/layout.tsx
@@ -1,5 +1,7 @@
 import "../globals.css";
 
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
@@ -159,6 +161,8 @@ export default async function RootLayout({
 					data-y_margin="18"
 					defer
 				/>
+				<Analytics />
+				<SpeedInsights />
 			</body>
 		</html>
 	);
