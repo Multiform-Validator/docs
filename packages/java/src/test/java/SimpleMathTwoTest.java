17b923b5786413096444a66a814a339240c0926b
gabriel-logan <gabrielcoutolima@protonmail.ch>
2024-07-06 07:52:23 -0300

    refactor: Rename SimpleMathTest to SimpleMathTwoTest

--- packages/java/src/test/java/SimpleMathTwoTest.java
+++ packages/java/src/test/java/SimpleMathTwoTest.java
@@ -1,7 +1,7 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class SimpleMathTest {
+class SimpleMathTwoTest {
     @Test
     void testAddition() {
         assertEquals(4, 3 + 1, "3 + 1 should equal 4");
