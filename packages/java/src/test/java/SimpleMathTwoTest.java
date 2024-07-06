5494fd3cb44be8649bd924bd73919d8b84ff05f8
gabriel-logan <gabrielcoutolima@protonmail.ch>
2024-07-06 07:50:57 -0300

    test: Add unit test for addition in SimpleMathTest

---
+++ packages/java/src/test/java/SimpleMathTwoTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SimpleMathTest {
+    @Test
+    void testAddition() {
+        assertEquals(4, 3 + 1, "3 + 1 should equal 4");
+    }
+}
