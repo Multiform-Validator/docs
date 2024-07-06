5c59c211e85a20717e931f456abd5f9f42d83a4b
gabriel-logan <gabrielcoutolima@protonmail.ch>
2024-07-06 01:50:48 -0300

    refactor: Add JUnit 5 dependencies for Java tests

---
+++ packages/java/src/tests/SimpleMathTest.java
@@ -0,0 +1,12 @@
+package io.multiform_validator; 
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SimpleMathTest {
+
+    @Test
+    public void testAddition() {
+        assertEquals(2, 1 + 1, "1 + 1 should equal 2");
+    }
+}
